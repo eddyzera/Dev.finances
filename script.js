01ba59b7e44388a34d27d5105f73b159104503cd
eddyzera <nescau23041996@gmail.com>
2021-05-04 17:42:56 -0300

    [create] integração com o servidor

--- script.js
+++ script.js
@@ -1,20 +1,71 @@
 function cancelForm() {
-    document.querySelector('.cancel').addEventListener('click', (event) => {
-        event.preventDefault()
-        document.querySelector('.modal-overlay').classList.remove('active')
-    })
-
-    document.querySelector('.modal-overlay').addEventListener('click', (event) => {
-        event.preventDefault()
-        document.querySelector('.modal-overlay').classList.remove('active')
-    } )    
+  document.querySelector(".cancel").addEventListener("click", (event) => {
+    event.preventDefault();
+    document.querySelector(".modal-overlay").classList.remove("active");
+  });
 }
 
 function displayForm() {
-    document.querySelector('.new').addEventListener('click', (event) => {
-        event.preventDefault()
-        document.querySelector('.modal-overlay').classList.add('active')
-    })
+  document.querySelector(".new").addEventListener("click", (event) => {
+    event.preventDefault();
+    document.querySelector(".modal-overlay").classList.add("active");
+  });
 }
-displayForm()
-cancelForm()
+
+function createTransaction() {
+  document.querySelector("#form-submit").addEventListener("submit", (event) => {
+    event.preventDefault();
+    let description = document.querySelector("#description").value;
+    let amount = document.querySelector("#amount").value;
+    let date = document.querySelector("#date").value;
+    fetch("http://localhost:1337/dev-finances", {
+      method: "POST",
+      body: JSON.stringify({
+        description: description,
+        value: amount,
+        date: date,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => response.json());
+  });
+}
+
+async function renderTransaction() {
+  const data = await fetchGet();
+  let render = document.querySelector("#render-data");
+  const tr = data.map((item) => {
+    return `
+                <tr>
+                    <td class="description">${item.description}</td>
+                    <td class="${
+                      Math.sign(item.value) == -1 ? "expense" : "income"
+                    }">R$: ${item.value.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                    })}</td>
+                    <td class="date">${new Date(item.date).toLocaleDateString(
+                      "pt-br"
+                    )}</td>
+                    <td>
+                        <img src="./assets/minus.svg" alt="remover transação">
+                    </td>
+                </tr>
+            `;
+  });
+
+  render.innerHTML = tr.join(" ");
+}
+
+async function fetchGet() {
+  const response = await fetch("http://localhost:1337/dev-finances", {
+    method: "GET",
+  });
+  const data = await response.json();
+  return data;
+}
+renderTransaction();
+displayForm();
+cancelForm();
+createTransaction();
